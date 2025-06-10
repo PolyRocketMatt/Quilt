1c2774400343a2d8a8af1a20c276369c4457986a
Matthias <rocketpencil.studios@gmail.com>
2025-06-11 00:11:15 +0200

    Add Roboto Font

--- src/quilt/main.py
+++ src/quilt/main.py
@@ -1,7 +1,7 @@
 import sys
 
 from PySide6.QtCore import Qt, QSize
-from PySide6.QtGui import QIcon
+from PySide6.QtGui import QIcon, QFont, QFontDatabase
 from PySide6.QtWidgets import *
 
 from src.quilt.ui.colors import COLORS
@@ -308,7 +308,18 @@ class QuiltMainWindow(QMainWindow):
 
 
 def main():
+    # Initialize the application
     app = QApplication(sys.argv)
+
+    # Load the font
+    font_id = QFontDatabase.addApplicationFont("assets/fonts/Roboto-Regular.ttf")
+    
+    if font_id == -1:
+        print("Failed to load font! Loading default font instead.")
+    else:
+        font_family = QFontDatabase.applicationFontFamilies(font_id)[0]
+        app.setFont(QFont(font_family, 10))
+
     quilt = QuiltMainWindow()
     quilt.show()
     sys.exit(app.exec())
