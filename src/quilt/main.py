37feeacbbeb636592df889f73674267a9ff77833
Matthias <rocketpencil.studios@gmail.com>
2025-06-11 04:15:56 +0200

    Add temporarly resizability

--- src/quilt/main.py
+++ src/quilt/main.py
@@ -1,7 +1,7 @@
 import sys
 
-from PySide6.QtCore import Qt, QSize
-from PySide6.QtGui import QIcon, QFont, QFontDatabase
+from PySide6.QtCore import Qt, QSize, QPoint, QRect
+from PySide6.QtGui import QIcon, QFont, QFontDatabase, QCursor
 from PySide6.QtWidgets import *
 
 from src.quilt.ui.utils import load_icon, load_colored_icon, load_and_save_padded_icon
@@ -22,6 +22,12 @@ class QuiltMainWindow(QMainWindow):
         self.setWindowTitle("Quilt Application")
         self.setGeometry(100, 100, 800, 600)
         self.setWindowFlags(Qt.FramelessWindowHint)
+
+        # Tracking
+        self._start_pos = None
+        self._resizing = False
+        self._resize_margin = 5
+        self._resize_edge = None
 
         # Set window icon
         self.setWindowIcon(QIcon("assets/quilt-nomid.ico"))
@@ -269,6 +275,103 @@ class QuiltMainWindow(QMainWindow):
 
     def _open_settings(self):
         QuiltNotImplementedPopup(self)
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self._start_pos = event.globalPos()
+            self._resize_edge = self._get_edge_at(event.pos())
+            self._resizing = self._resize_edge is not None
+            if self._resizing:
+                event.accept()
+            else:
+                super().mousePressEvent(event)
+
+    def mouseReleaseEvent(self, event):
+        self._resizing = False
+        self._resize_edge = None
+        super().mouseReleaseEvent(event)
+
+    def mouseMoveEvent(self, event):
+        if self._resizing and self._resize_edge:
+            self._resize_window(event.globalPos())
+            event.accept()
+        else:
+            edge = self._get_edge_at(event.pos())
+            self._update_cursor(edge)
+            super().mouseMoveEvent(event)
+
+    def _get_edge_at(self, pos):
+        rect = self.rect()
+        x, y = pos.x(), pos.y()
+        margin = self._resize_margin
+
+        left = x < margin
+        right = x > rect.width() - margin
+        top = y < margin
+        bottom = y > rect.height() - margin
+
+        if top and left:
+            return 'top_left'
+        elif top and right:
+            return 'top_right'
+        elif bottom and left:
+            return 'bottom_left'
+        elif bottom and right:
+            return 'bottom_right'
+        elif top:
+            return 'top'
+        elif bottom:
+            return 'bottom'
+        elif left:
+            return 'left'
+        elif right:
+            return 'right'
+        return None
+
+    def _resize_window(self, global_pos):
+        diff = global_pos - self._start_pos
+        geom = self.geometry()
+
+        if 'left' in self._resize_edge:
+            new_x = geom.x() + diff.x()
+            new_w = geom.width() - diff.x()
+            if new_w > self.minimumWidth():
+                geom.setX(new_x)
+                geom.setWidth(new_w)
+
+        if 'right' in self._resize_edge:
+            new_w = geom.width() + diff.x()
+            if new_w > self.minimumWidth():
+                geom.setWidth(new_w)
+
+        if 'top' in self._resize_edge:
+            new_y = geom.y() + diff.y()
+            new_h = geom.height() - diff.y()
+            if new_h > self.minimumHeight():
+                geom.setY(new_y)
+                geom.setHeight(new_h)
+
+        if 'bottom' in self._resize_edge:
+            new_h = geom.height() + diff.y()
+            if new_h > self.minimumHeight():
+                geom.setHeight(new_h)
+
+        self.setGeometry(geom)
+        self._start_pos = global_pos
+
+    def _update_cursor(self, edge):
+        cursors = {
+            'top_left': Qt.SizeFDiagCursor,
+            'bottom_right': Qt.SizeFDiagCursor,
+            'top_right': Qt.SizeBDiagCursor,
+            'bottom_left': Qt.SizeBDiagCursor,
+            'top': Qt.SizeVerCursor,
+            'bottom': Qt.SizeVerCursor,
+            'left': Qt.SizeHorCursor,
+            'right': Qt.SizeHorCursor,
+        }
+        cursor = cursors.get(edge, Qt.ArrowCursor)
+        self.setCursor(cursor)
 
 def main():
     # Initialize the application
